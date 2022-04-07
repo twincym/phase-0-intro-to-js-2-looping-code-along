358fbd1456ba36355d5bd24f4f0dcdb86bc4d467
twincy modi <twincymodi@gmail.com>
2022-04-07 00:55:28 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+let cards = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(cards){
+    const newCards = []
+for (let i = 0; i< cards.length; i++) {
+        let message = `Thank you, ${cards[i]}, for the wonderful surprise gift!`;
+        newCards.push(message)
+    }
+    return newCards
+}
+
+let countingDown = 12
+function countDown(countingDown){
+    while (countingDown > -1) {
+        console.log(countingDown--);
+    }
+}
